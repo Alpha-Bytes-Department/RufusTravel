a3a3f34dd56bebde2d6f2a3d1678dae6c70c4f50
Joy Sutradhar <joysd.1005@fec.edu.bd>
2025-12-03 04:57:11 +0600

    project innitialization commit

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=' text-center font-bold text-3xl'><p className='py-32'>Log in page</p> </div>
+  )
+}
+
+export default page
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=" text-center font-bold text-3xl">
+      <p className="py-32">Sign up page</p>{" "}
+    </div>
+  );
+}
+
+export default page
